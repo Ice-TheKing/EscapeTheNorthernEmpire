528186c068588fb6e796deb1a43d3de03366e795
Nic Hartley <nxh9052@rit.edu>
2019-03-18 15:42:51 -0400

    feature: added force shot, only slightly broken

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    public float Speed;
+    public float Lifetime;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.right * Speed;
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.right);
+        Lifetime -= Time.deltaTime;
+        if (Lifetime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,23 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour {
+public class PlayerControls : MonoBehaviour
+{
     const float BASIC_SCALING_FACTOR = 10.0f;
 
     public float MoveSpeed = 1.0f;
     public float JumpSpeed = 10.0f;
 
+    public Bullet ForceShot;
+
     Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.A))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(Vector2.left * BASIC_SCALING_FACTOR * MoveSpeed);
             transform.localScale = new Vector3(-1, 1);
@@ -31,9 +36,16 @@ public class PlayerControls : MonoBehaviour {
         {
             rb.AddForce(Vector2.up * BASIC_SCALING_FACTOR * JumpSpeed);
         }
+
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("I'm a firin' mah lazor");
+            var target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+            var relativeTarget = target - transform.position;
+            var angle = Mathf.Atan2(relativeTarget.y, relativeTarget.x);
+            var direction = Quaternion.AngleAxis(angle, Vector3.forward);
+            var bullet = Instantiate(ForceShot, transform.position, direction);
+            Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
-	}
+    }
 }
+
