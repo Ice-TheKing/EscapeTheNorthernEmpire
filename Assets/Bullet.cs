e5d35ce4ef6cb5d0eb8ddc2f291b97447ddc4d1c
Nic Hartley <nxh9052@rit.edu>
2019-03-18 18:23:12 -0400

    tweaked stuff, added destructibility

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -22,4 +22,12 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Destructible") {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
+    }
 }
