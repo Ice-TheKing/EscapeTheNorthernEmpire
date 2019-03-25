473013cb19a7b9bd5555d3416d9bfcab8eaddc1a
Nic Hartley <nxh9052@rit.edu>
2019-03-25 19:07:31 -0400

    added parallax script and demoed it in the first level

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float ScrollScale;
+    public Camera tracking;
+
+    private float lastX;
+
+    public float diff;
+    
     void Start()
     {
-        
+        if (tracking == null) tracking = Camera.main;
+        lastX = tracking.transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        diff = tracking.transform.position.x - lastX;
+        transform.position += new Vector3(diff * ScrollScale, 0);
+        lastX = tracking.transform.position.x;
     }
 }
